9a30542fa30b9f6546f2dd9cc7f0169dda9dfe67
Serhii Smoliak <serhiysmoliak@gmail.com>
2025-04-05 23:13:26 +0300

    fix bug in task-3 and add test cases

--- HW-2b/js/task-3.js
+++ HW-2b/js/task-3.js
@@ -8,6 +8,14 @@ const profile = {
         this.playTime += hours;
     },
     getInfo() {
-        return `${this.username} has ${playTime} active hours`;
+        return `${this.username} has ${this.playTime} active hours!`;
     },
 };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
