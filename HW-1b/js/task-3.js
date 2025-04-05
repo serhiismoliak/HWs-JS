164747f7fa9ff810925c2aed2a1072a713096472
Serhii Smoliak <serhiysmoliak@gmail.com>
2025-04-05 12:56:58 +0300

    Merged part two of first homework into main branch

---
+++ HW-1b/js/task-3.js
@@ -0,0 +1,20 @@
+function checkForSpam(message) {
+    message = message.toLowerCase();
+    const tokens = ['spam', 'sale'];
+    const result = tokens.some((token) => message.includes(token));
+    return result
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+console.log(checkForSpam("Get best sale offers now!")); // true
+
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
